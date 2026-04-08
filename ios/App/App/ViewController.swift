01785bce438f8765d5ae11582f1d969ed08cb6f0
moyu100100-png <moyu100100@gmail.com>
2026-04-08 18:29:05 +0900

    fix: ViewController camera permission compatible with Capacitor

--- ios/App/App/ViewController.swift
+++ ios/App/App/ViewController.swift
@@ -3,7 +3,17 @@ import Capacitor
 import WebKit
 
 class ViewController: CAPBridgeViewController {
-    override func webView(_ webView: WKWebView, requestMediaCapturePermissionFor origin: WKSecurityOrigin, initiatedByFrame frame: WKFrameInfo, type: WKMediaCaptureType, decisionHandler: @escaping (WKPermissionDecision) -> Void) {
-        decisionHandler(.grant)
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        if #available(iOS 15.0, *) {
+            webView?.configuration.defaultWebpagePreferences.allowsContentJavaScript = true
+        }
+    }
+
+    override func webViewConfiguration() -> WKWebViewConfiguration {
+        let config = super.webViewConfiguration()
+        config.allowsInlineMediaPlayback = true
+        config.mediaTypesRequiringUserActionForPlayback = []
+        return config
     }
 }
