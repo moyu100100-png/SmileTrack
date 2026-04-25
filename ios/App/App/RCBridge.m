f9890bfb3ab01bf85d2f45735513732037a2e27f
moyu100100-png <moyu100100@gmail.com>
2026-04-25 11:53:29 +0900

    fix: remove RCBridge, use official purchases-capacitor

--- ios/App/App/RCBridge.m
+++
@@ -1,9 +0,0 @@
-#import <Foundation/Foundation.h>
-#import <Capacitor/Capacitor.h>
-
-CAP_PLUGIN(RCBridge, "RCBridge",
-    CAP_PLUGIN_METHOD(getOfferings, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(purchasePackage, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(restorePurchases, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(getCustomerInfo, CAPPluginReturnPromise);
-)
